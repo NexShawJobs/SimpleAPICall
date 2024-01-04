7a9df611587d382982d770895b16ef51397fe636
NexShawJobs <nex.shaw.jobs@gmail.com>
2024-01-04 05:04:56 -0500

    Initial Commit

---
+++ SimpleAPICall/SimpleAPICallApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAPICallApp.swift
+//  SimpleAPICall
+//
+//  Created by NebSha on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAPICallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
