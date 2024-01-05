c6bfba720cc86f9d383b7228d9151f91ea380efe
NexShawJobs <nex.shaw.jobs@gmail.com>
2024-01-05 01:21:09 -0500

    add the network call

--- SimpleAPICall/ContentView.swift
+++ SimpleAPICall/ContentView.swift
@@ -8,22 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var user:GitHubUser?
     var body: some View {
         VStack {
-            Circle()
-                .foregroundColor(.secondary)
-                .frame(width: 120, height: 120)
-            Text("Username")
+
+            AsyncImage(url: URL(string: user?.avatarUrl ?? " ")) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+            } placeholder: {
+                Circle()
+                    .foregroundColor(.secondary)
+            }
+            .frame(width: 120, height: 120)
+
+
+            Text(user?.login ?? "Login place holder.")
                 .bold()
                 .font(.title3)
-            Text("This is where the Github bio will go. lets make it long so it spans two lines.")
-                .padding()
+//            Text(user?.bio ?? "Bio place holder")
+//                .padding()
             Spacer()
         }
         .padding()
+        .task {
+            do {
+                user = try await getUser()
+            }  catch {
+                GHError.invalidUrl
+                print("Invalid Url")
+            } catch {
+                GHError.invalidData
+                print("Invalid Data")
+            } catch {
+                GHError.invalidResponse
+                print("Invalid Response")
+            } catch {
+                print("Unexpected Eerror")
+            }
+            
+        }
     }
     func getUser() async throws -> GitHubUser {
         let endpoint = "https://api.github.com/users/NexShawJobs" //sallen0400
+    // let endpoint = "https://api.github.com/users/sallen0400"
+
         guard let url = URL(string: endpoint) else {
             throw GHError.invalidUrl
         }
@@ -35,6 +65,7 @@ struct ContentView: View {
         do {
             let decoder = JSONDecoder()
             decoder.keyDecodingStrategy = .convertFromSnakeCase
+            print(decoder)
             return try decoder.decode(GitHubUser.self, from: data)
         } catch {
             throw GHError.invalidData
@@ -56,7 +87,7 @@ enum GHError: Error {
 
 struct GitHubUser: Codable {
     let login: String
-    let avatar_url: String
-    let bio: String
+    let avatarUrl: String
+    //let bio: String
     
 }
